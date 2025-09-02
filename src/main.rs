99916d3fea9d7284dc4dd6ef7911dadeeaf96086
Azmi Sahin <6445951+azmisahin@users.noreply.github.com>
2025-09-03 02:04:26 +0300

    fix: dep

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use sentiric_media_service::run;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // Tüm rustls ile ilgili manuel kurulum kodlarını sildik.
-    // Sorunu Cargo.toml'da çözüyoruz.
+
+    
     run().await
 }
