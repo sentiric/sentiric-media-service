a3db3b689f8e17169af320cdf59d8bfd5dcc8dd7
Azmi Sahin <6445951+azmisahin@users.noreply.github.com>
2025-09-03 02:35:39 +0300

    fix: rustls 0.23.31

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use rustls::crypto::CryptoProvider;
+use rustls::crypto::ring::default_provider; // Ring provider'ı import et
 use sentiric_media_service::run;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-
+    // Ring crypto provider'ı kullan
+    let provider = default_provider();
+    CryptoProvider::install_default(provider).expect("Failed to install crypto provider");
     
     run().await
 }
